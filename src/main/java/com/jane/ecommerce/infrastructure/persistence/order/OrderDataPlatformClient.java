883d7fecc8b184d71e5a49c9945a1d1c68065de8
jane <janelee1721@gmail.com>
2025-01-16 21:18:31 +0900

    🐛Fix: Missing @Service in OrderDataPlatformClient class

--- src/main/java/com/jane/ecommerce/infrastructure/persistence/order/OrderDataPlatformClient.java
+++ src/main/java/com/jane/ecommerce/infrastructure/persistence/order/OrderDataPlatformClient.java
@@ -2,7 +2,9 @@ package com.jane.ecommerce.infrastructure.persistence.order;
 
 import com.jane.ecommerce.domain.order.Order;
 import com.jane.ecommerce.domain.order.OrderClient;
+import org.springframework.stereotype.Service;
 
+@Service
 public class OrderDataPlatformClient implements OrderClient {
 
     @Override
