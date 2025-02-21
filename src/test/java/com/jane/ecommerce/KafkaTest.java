7db5eedb5ad275e76ac6f2946498f043a23a6cbd
jane <janelee1721@gmail.com>
2025-02-21 16:32:38 +0900

    ✅Test: Add Kafka integration test for producer and consumer
    - 메세지 내용을 검증하는 Assertion 추가

--- src/test/java/com/jane/ecommerce/KafkaTest.java
+++ src/test/java/com/jane/ecommerce/KafkaTest.java
@@ -1,5 +1,6 @@
 package com.jane.ecommerce;
 
+import java.util.concurrent.atomic.AtomicReference;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,6 +17,7 @@ import static org.awaitility.Awaitility.await;
 public class KafkaTest {
     private static final String TOPIC = "test";
     private static final AtomicInteger counter = new AtomicInteger(0);
+    private static final AtomicReference<String> receivedMessage = new AtomicReference<>(); // 수신된 메시지 저장
 
     @Autowired
     private KafkaTemplate<String, Object> producer;
@@ -23,17 +25,23 @@ public class KafkaTest {
     @KafkaListener(topics = TOPIC)
     public void listen(String message) {
         counter.incrementAndGet(); // 리스너가 호출되면 1 증가
+        receivedMessage.set(message); // 수신된 메시지 저장
     }
 
     @Test
     void kafkaTest() {
-        producer.send(TOPIC, "Hello, Jane!");
+        String expectedMessage = "Hello, Jane!";
+        producer.send(TOPIC, expectedMessage);
+
         await() // 검증될 때 까지 기다린다.
                 .pollInterval(Duration.ofMillis(300)) // 300ms 마다 한 번씩
                 .atMost(Duration.ofSeconds(2)) // 최대 2s 까지
                 .untilAsserted(() -> { // 아래 모든 assertion 이 만족할 때 까지
                     System.out.println("counter.get() = " + counter.get());
+                    System.out.println("receivedMessage.get() = " + receivedMessage.get());
+
                     assertThat(counter.get()).isEqualTo(1L);
+                    assertThat(receivedMessage.get().replaceAll("^\"|\"$", "")).isEqualTo(expectedMessage); // 메시지 일치 여부 확인
                 });
     }
 }
